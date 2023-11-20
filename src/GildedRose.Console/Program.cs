7baea4f4442f0c8808284cc1c7b3af4750e6c9d3
Stijn van der Linden <stijnvanderlinden@live.nl>
2023-11-20 16:44:22 +0100

    Refactor

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using GildedRose.Console.Models;
-using GildedRose.Console.Enums;
-using System.Collections.Generic;
-using System.Linq;
-using System;
 using GildedRose.Console.Repositories;
 
 namespace GildedRose.Console
@@ -24,15 +20,12 @@ namespace GildedRose.Console
 
             System.Console.ReadKey();
 
-            while (app.repo.GetItems().Where(x => x.Quality > 0).Any())
+            foreach (Item item in app.repo.GetItems())
             {
-                foreach (Item item in app.repo.GetItems())
-                {
-                    app.repo.UpdateQuality(item);
-                    System.Console.WriteLine(item);
-                }
-                System.Console.ReadKey();
+                app.repo.UpdateQuality(item);
+                System.Console.WriteLine(item);
             }
+            System.Console.ReadKey();
         }
     }
 }
